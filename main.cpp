e384b001dd57c90169dd88f7cb3a5b0c578806da
Dmitry Popov <dmitry.popov@saritasa.com>
2020-11-12 19:46:49 +0700

    Add fast_pow implementation

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
-double my_pow(double value, unsigned int pow) {
-    double result = 1.0;
-    for (unsigned int i = 0; i < pow; i++) {
-        result *= value;
-    }
-    return result;
+double fast_pow(double value, unsigned int pow) {
+   double result = 1.0;
+   while (pow) {
+       if (pow %  == 1) {
+           result *= value;
+       }
+
+       value *= value;
+       pow /= 2;
+   }
+   return result;
 }
