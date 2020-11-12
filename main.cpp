fc7ba5a2c2314c843dd7febe0926538c1a64f07b
Dmitry Popov <dmitry.popov@saritasa.com>
2020-11-12 19:50:27 +0700

    Implement support of negative numbers

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
-double my_pow(double value, unsigned int pow) {
+double my_pow(double value, int pow) {
+    int lhs;
+    int rhs;
+
+    if (pow < 0) {
+        lhs = pow;
+        rhs = 0;
+    } else {
+        lhs = 0;
+        rhs = pow;
+    }
+
     double result = 1.0;
-    for (unsigned int i = 0; i < pow; i++) {
+    for (int i = lhs; i < rhs; i++) {
         result *= value;
     }
-    return result;
+
+    return pow < 0 ? (1.0 / result) : result;
 }
